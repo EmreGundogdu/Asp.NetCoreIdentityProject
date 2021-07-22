be57572daaec2d707903d77794e345727e640800
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-07-22 14:44:20 +0300

    Views, models & controller added

--- Asp.NetCoreIdentity/Startup.cs
+++ Asp.NetCoreIdentity/Startup.cs
@@ -1,13 +1,16 @@
 using Asp.NetCoreIdentity.Context;
+using Asp.NetCoreIdentity.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,7 +27,8 @@ namespace Asp.NetCoreIdentity
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationContext>(options=>
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<ApplicationContext>();
+            services.AddDbContext<ApplicationContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("SqlConnection"));
             });
@@ -38,6 +42,13 @@ namespace Asp.NetCoreIdentity
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                RequestPath = "/node_modules",
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules"))
+            });
 
             app.UseRouting();
 
