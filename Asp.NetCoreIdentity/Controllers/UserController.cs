e5b5e0c388d881c0c4b13b469fdcbf2b26a580fd
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-07-25 22:39:27 +0300

    User List

--- Asp.NetCoreIdentity/Controllers/UserController.cs
+++ Asp.NetCoreIdentity/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Asp.NetCoreIdentity.Context;
+using Asp.NetCoreIdentity.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,11 +10,45 @@ using System.Threading.Tasks;
 
 namespace Asp.NetCoreIdentity.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ApplicationContext _conext;
+
+        public UserController(UserManager<AppUser> userManager, ApplicationContext conext)
         {
+            _userManager = userManager;
+            _conext = conext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var query = _userManager.Users;
+            var users = _conext.Users.Join(_conext.UserRoles, user => user.Id, userRole => userRole.UserId, (user, userRole) => new
+            {
+                user,
+                userRole
+            }).Join(_conext.Roles,two=>two.userRole.RoleId,role=>role.Id,(two,role)=>new { two.user,two.userRole,role}).Where(x => x.userRole.RoleId != 3).Select(x => new AppUser()
+            {
+                Id = x.user.Id,
+                AccessFailedCount = x.user.AccessFailedCount,
+                ConcurrencyStamp = x.user.ConcurrencyStamp,
+                Email = x.user.Email,
+                Gender = x.user.Gender,
+                ImagePath = x.user.ImagePath,
+                LockoutEnabled = x.user.LockoutEnabled,
+                LockoutEnd = x.user.LockoutEnd,
+                NormalizedEmail = x.user.NormalizedEmail,
+                NormalizedUserName = x.user.NormalizedUserName,
+                PasswordHash = x.user.PasswordHash,
+                PhoneNumber = x.user.PhoneNumber,
+                UserName = x.user.UserName
+
+            }).ToList();
+
+            var usersII = await _userManager.GetUsersInRoleAsync("Member");
+
             return View();
         }
     }
